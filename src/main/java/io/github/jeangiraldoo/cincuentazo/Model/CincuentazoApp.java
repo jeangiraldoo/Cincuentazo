dd0a543acca6c000d4cab3f30a116ca5121dd9a7
Emerson Albornoz <emerson02325@gmail.com>
2024-12-07 23:27:40 -0500

    feat: provisional class for running the game

---
+++ src/main/java/io/github/jeangiraldoo/cincuentazo/Model/CincuentazoApp.java
@@ -0,0 +1,21 @@
+package io.github.jeangiraldoo.cincuentazo.Model;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class CincuentazoApp extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/cincuentazo/hello-view.fxml"));
+        Scene scene = new Scene(loader.load());
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Cincuentazo");
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
