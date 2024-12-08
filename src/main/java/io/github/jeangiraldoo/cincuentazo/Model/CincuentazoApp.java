0404379816fa2eb57afe9bef86d036c8123f72d8
Emerson Albornoz <emerson02325@gmail.com>
2024-12-07 23:40:24 -0500

    fix: modularity errors

--- src/main/java/io/github/jeangiraldoo/cincuentazo/Model/CincuentazoApp.java
+++ src/main/java/io/github/jeangiraldoo/cincuentazo/Model/CincuentazoApp.java
@@ -8,7 +8,7 @@ import javafx.stage.Stage;
 public class CincuentazoApp extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/cincuentazo/hello-view.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/io/github/jeangiraldoo/cincuentazo/hello-view.fxml"));
         Scene scene = new Scene(loader.load());
         primaryStage.setScene(scene);
         primaryStage.setTitle("Cincuentazo");
